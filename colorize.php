c208825298adc02b8f54203d03ff0b96f6387f7a
snoj <josh@snoj.us>
2022-02-12 23:13:32 -0600

    Speed up and simplified code
    Signed-off-by: snoj <josh@snoj.us>

--- colorize.php
+++ colorize.php
@@ -1,7 +1,9 @@
 #!/usr/bin/php
 <?php
 
-$byteArray = unpack("L*",file_get_contents('./ColoringPage06.jpg'));
+ini_set('memory_limit', '1G');
+
+$byteArray = unpack('L*', file_get_contents('./ColoringPage06.jpg'));
 $arrlength = count($byteArray);
 
 $img = imagecreatefrompng('./fix-canvas.png');
@@ -16,28 +18,30 @@ $progress_c = 2550 * 3300;
 $progress_p = 0;
 $last_percent = 0;
 
-for($wi = 0; $wi < 2550; $wi++) {
-  for($hi = 0; $hi < 3300; $hi++) {
-    $progress_p = floor(($progress_i/$progress_c) * 100);
-    if($progress_p % 10 == 0 && $progress_p != $last_percent) {
-      imagepng($img, "./fill-finished.png");
-      $last_percent = $progress_p;
-    }
+for ($i = 0; $i < $progress_c; $i++) {
+    $wi = floor($i / 3300);
+    $hi = $i % 3300;
 
-    $progress_line = "                                                                                                                         \r".
-                      (($progress_i/$progress_c) * 100). "%";
-    fwrite(STDERR, $progress_line);
-    
     $new_colorat_ref = imagecolorat($img, $wi, $hi);
     $new_colorat = imagecolorsforindex($img, $new_colorat_ref);
-    if(sprintf("%02x%02x%02x", $new_colorat["red"], $new_colorat["green"], $new_colorat["blue"]) != "ffffff") {
-      $progress_i++;
-      continue;
+    if($i  % ($progress_c / 10) == 0) {
+      echo "\r" . round($i / $progress_c * 100 , 2) . "% complete";
     }
     
-    $offset = ($wi * $hi) % $arrlength;
-    if($offset == 0){
-      $offset = 1;
+    if (
+        sprintf(
+            '%02x%02x%02x',
+            $new_colorat['red'],
+            $new_colorat['green'],
+            $new_colorat['blue']
+        ) != 'ffffff'
+    ) {
+        continue;
+    }
+
+    $offset = $wi * $hi % $arrlength;
+    if ($offset == 0) {
+        $offset = 1;
     }
 
     $rgba = unpack('C*', pack('L', $byteArray[$offset]));
@@ -45,12 +49,10 @@ for($wi = 0; $wi < 2550; $wi++) {
     $colorat_ref = imagecolorat($img_ori, $wi, $hi);
     $colorat = imagecolorsforindex($img_ori, $colorat_ref);
 
-    if($colorat["red"] > 0)
-      imagefill($img, $wi, $hi, $colra);
-
-    $progress_i++;
-  }
+    if ($colorat['red'] > 0) {
+        imagefill($img, $wi, $hi, $colra);
+    }
 }
-
-echo "\n";
-imagepng($img, "./fill-finished.png");
+echo "\r" . round($i / $progress_c * 100 , 2) . "% complete";
+imagepng($img, './fill-finished.png');
+echo "\nfinished";
--- fix-blackwhite.php
+++ fix-blackwhite.php
@@ -1,6 +1,5 @@
 #!/usr/bin/php
 <?php
-
 $img_ori = imagecreatefromjpeg('./ColoringPage06.jpg');
 $img = imagecreatetruecolor(2550, 3300);
 $progress_i = 0;
@@ -9,28 +8,23 @@ $progress_p = 0;
 $last_percent = 0;
 $color_black = imagecolorallocatealpha($img_ori, 0, 0, 0, 0);
 $color_white = imagecolorallocatealpha($img_ori, 0xff, 0xff, 0xff, 0);
-for($wi = 0; $wi < 2550; $wi++) {
-  for($hi = 0; $hi < 3300; $hi++) {
-    $progress_p = floor(($progress_i/$progress_c) * 100);
-    $progress_line = "                                                                                                                         \r".
-                      (($progress_i/$progress_c) * 100). "%";
-    fwrite(STDERR, $progress_line);
+for ($i = 0; $i < $progress_c; $i++) {
+    $wi = floor($i / 3300);
+    $hi = $i % 3300;
 
     $colorat_ref = imagecolorat($img_ori, $wi, $hi);
     $colorat = imagecolorsforindex($img_ori, $colorat_ref);
-
-    if(min($colorat["red"], $colorat["green"], $colorat["blue"]) > 40)
-      imagesetpixel($img, $wi, $hi, $color_white);
-    else
-      imagesetpixel($img, $wi, $hi, $color_black);
-
-    if($progress_p % 10 == 0 && $progress_p != $last_percent) {
-      imagepng($img, "./fix-canvas.png");
-      $last_percent = $progress_p;
+    if (min($colorat['red'], $colorat['green'], $colorat['blue']) > 40) {
+        imagesetpixel($img, $wi, $hi, $color_white);
+    } else {
+        imagesetpixel($img, $wi, $hi, $color_black);
     }
 
-    $progress_i++;
-  }
+    if($i  % ($progress_c / 10) == 0) {
+      echo "\r" . round($i / $progress_c * 100 , 2) . "% complete";
+    }
 }
-echo "\n";
-imagepng($img, "./fix-canvas.png");
+echo "\r" . round($i / $progress_c * 100 , 2) . "% complete";
+
+imagepng($img, './fix-canvas.png');
+echo "\r\nfinished";
